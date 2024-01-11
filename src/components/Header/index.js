0a50a47382dcc39f9dc7f7699199324d35e82fc8
jayakrishnavamsi24 <jayakrishnavamsi24@gmail.com>
2024-01-11 07:06:58 +0000

    pushed code

---
+++ src/components/Header/index.js
@@ -0,0 +1,84 @@
+import {Link, withRouter} from 'react-router-dom'
+import {AiFillHome} from 'react-icons/ai'
+import {BsBriefcaseFill} from 'react-icons/bs'
+import {FiLogOut} from 'react-icons/fi'
+
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+const Header = props => {
+  const onClickLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <div className="nav-bar-mobile-logo-container">
+          <Link to="/">
+            <img
+              className="website-logo"
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+              alt="website logo"
+            />
+          </Link>
+          <div className="mobile-btn-container">
+            <Link to="/" className="nav-link">
+              <AiFillHome style={{color: '#ffffff', fontSize: '22'}} />
+            </Link>
+            <Link to="/jobs" className="nav-link">
+              <BsBriefcaseFill
+                style={{color: '#ffffff', fontSize: '22', marginLeft: '20'}}
+              />
+            </Link>
+            <button
+              type="button"
+              aria-label="logout"
+              className="nav-mobile-btn"
+            >
+              <FiLogOut
+                style={{color: '#ffffff', fontSize: '22', marginLeft: '20'}}
+                onClick={onClickLogout}
+              />
+            </button>
+          </div>
+        </div>
+
+        <div className="nav-bar-large-container">
+          <Link to="/">
+            <img
+              className="website-logo"
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+              alt="website logo"
+            />
+          </Link>
+          <ul className="nav-menu">
+            <li className="nav-menu-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+
+            <li className="nav-menu-item">
+              <Link to="/jobs" className="nav-link">
+                Jobs
+              </Link>
+            </li>
+          </ul>
+          <button
+            type="button"
+            className="logout-desktop-btn"
+            onClick={onClickLogout}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
